853d6f6e5ab0b16e6e437f13b1f3ffd7e27183ef
Carlos <carloscombatt17@gmail.com>
2021-11-29 15:38:48 -0500

    Front

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './usuario/login/login.component';
+import { RegistrarComponent } from './usuario/registrar/registrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: LoginComponent
+  },
+  {
+    path: 'registrarse',
+    component: RegistrarComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
